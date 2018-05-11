7483e0612fcba35206db13e8efb006054ec90031
benhjt <benhjt@users.noreply.github.com>
2018-05-11 14:10:22 +0100

    Include satellite import

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,6 +1,7 @@
 /* eslint-env node */
 
 module.exports = {
+  replacePrefix: "/iss-tracker/",
   staticFileGlobs: [
     'bower_components/webcomponentsjs/webcomponents-loader.js',
     'manifest.json',
